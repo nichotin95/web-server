8e9b746c23c8dd2cf569d5dd4230a24874aed8a0
Nitin Chopra <nitin95chopra@gmail.com>
2019-10-08 19:15:18 +0530

    Ag

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
-var middleware = {
-	requireAuthentication: function (req, res, next) {
-		console.log('private route hit!');
-		next();
-	},
-	logger: function (req, res, next) {
-		console.log('Request: ' + new Date() + ' ' + req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
-
+const PORT = 3001;
+const middleware = require("./middleware");
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
@@ -21,5 +11,5 @@ app.get('/about', middleware.requireAuthentication, function (req, res) {
 app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function () {
-	console.log('Express server started on port ' + PORT + '!');
+	console.log('Expressss server started on port ' + PORT + '!');
 });
